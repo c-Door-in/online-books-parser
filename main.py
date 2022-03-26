6ef0cc98f9d7247456a14a7d407899d9411fc111
c_Door in <egor1@mail.ru>
2022-03-26 23:37:50 +0300

    Download book covers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+from urllib.parse import urljoin, urlsplit, unquote
 
 import requests
 import urllib3
@@ -20,41 +21,72 @@ def fetch_book(url):
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(url, response)
-    return response
+    return response.text
 
 
-def get_title_and_author(book_title_url):
-    response = fetch_book(book_title_url)
-    soup = BeautifulSoup(response.text, 'lxml')
+def get_title_and_author(soup):
     title_tag = soup.find('div', id='content').find('h1')
     title, author = title_tag.text.split('::')
     return title.strip(), author.strip()
 
 
-def compose_filename(book_id, book_title_url):
-    title, author = get_title_and_author(book_title_url)
-    return f'{book_id}. {title}.txt'
+def compose_text_filename(book_title, book_id):
+    soup = BeautifulSoup(book_title, 'lxml')
+    title, author = get_title_and_author(soup)
+    filename = sanitize_filename(f'{book_id}. {title}.txt')
+    return filename
 
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(book_text, filename, folder='books/'):
     Path(folder).mkdir(parents=True, exist_ok=True)
-    filename = sanitize_filename(filename)
-    response = fetch_book(url)
     filepath = os.path.join(folder, filename)
     with open(filepath, 'w') as file:
-        file.write(response.text)
+        file.write(book_text)
     return filepath
+
+
+def get_image_url(soup):
+    image_tag = soup.find('div', class_='bookimage').find('img')
+    return image_tag['src']
+
+
+def fetch_image(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
+
+
+def download_image(title_url, book_title, folder='images/'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    soup = BeautifulSoup(book_title, 'lxml')
+    image_url = urljoin(title_url, get_image_url(soup))
+    image = fetch_image(image_url)
+    filename = os.path.basename(unquote(urlsplit(image_url).path))
+    filepath = os.path.join(folder, filename)
+    with open(filepath, 'wb') as file:
+        file.write(image)
+    return filepath
+
+
+def download_book(book_id):
+    text_url = f'https://tululu.org/txt.php?id={book_id}'
+    book_text = fetch_book(text_url)
+
+    title_url = f'https://tululu.org/b{book_id}/'
+    book_title = fetch_book(title_url)
+
+    text_filename = compose_text_filename(book_title, book_id)
+    download_txt(book_text, text_filename)
+    download_image(title_url, book_title)
+
 
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     count = 10
     for book_id in range(1, count+1):
-        book_title_url = f'https://tululu.org/b{book_id}/'
-        text_url = f'https://tululu.org/txt.php?id={book_id}'
         try:
-            filename = compose_filename(book_id, book_title_url)
-            download_txt(text_url, filename)
+            download_book(book_id)
         except requests.HTTPError:
             continue
 

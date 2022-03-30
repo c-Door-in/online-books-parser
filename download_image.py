067624906ddac867b67d5ec780b140ced2f74a76
c_Door in <egor1@mail.ru>
2022-03-31 00:27:48 +0300

    Return requests back to the parent functions

--- download_image.py
+++ download_image.py
@@ -5,18 +5,14 @@ from urllib.parse import urljoin, urlsplit, unquote
 import requests
 
 
-def fetch_image(url):
+def download_image(title_url, image_url, folder='images/'):
+    url = urljoin(title_url, image_url)
     response = requests.get(url)
     response.raise_for_status()
-    return response.content
 
-
-def download_image(title_url, image_url, folder='images/'):
     Path(folder).mkdir(parents=True, exist_ok=True)
-    url = urljoin(title_url, image_url)
-    image = fetch_image(url)
     filename = os.path.basename(unquote(urlsplit(image_url).path))
     filepath = os.path.join(folder, filename)
     with open(filepath, 'wb') as file:
-        file.write(image)
+        file.write(response.content)
     return filepath
--- download_txt.py
+++ download_txt.py
@@ -6,22 +6,18 @@ from urllib.parse import urljoin
 from pathvalidate import sanitize_filename
 
 
-def fetch_book(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
-
-
 def get_filepath(folder, title, id):
     filename = sanitize_filename(f'{id}. {title}.txt')
     return os.path.join(folder, filename)
 
 
 def download_txt(title_url, txt_url, id, title, folder='books/'):
+    url = urljoin(title_url, txt_url)
+    response = requests.get(url)
+    response.raise_for_status()
+
     Path(folder).mkdir(parents=True, exist_ok=True)
     filepath = get_filepath(folder, title, id)
-    url = urljoin(title_url, txt_url)
-    book_text = fetch_book(url)
     with open(filepath, 'w') as file:
-        file.write(book_text)
+        file.write(response.text)
     return filepath

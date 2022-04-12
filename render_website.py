a0e4ff0c038803df6434348bac65dcb526c01caf
c_Door in <egor1@mail.ru>
2022-04-12 17:32:08 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -3,29 +3,40 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib.parse import urljoin
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
+def get_index_template():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    return env.get_template('template.html')
 
-template = env.get_template('template.html')
-
-with open('./parsing_result/books.json', 'r', encoding='utf8') as file:
-    books = json.load(file)
-
-category_url = 'https://tululu.org/l55/'
-for book in books:
-    book['image_url'] = urljoin(category_url, book['image_url'])
+# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+# server.serve_forever()
 
 
-rendered_page = template.render(
-    books=books,
-)
+def main():
+    with open('./parsing_result/books.json', 'r', encoding='utf8') as file:
+        books = json.load(file)
+    category_url = 'https://tululu.org/l55/'
+    for book in books:
+        book['image_url'] = urljoin(category_url, book['image_url'])
 
-with open('index.html', 'w', encoding='utf8') as file:
-    file.write(rendered_page)
+    
+    def on_reload():
+        rendered_page = get_index_template().render(
+            books=books,
+        )
+        with open('index.html', 'w', encoding='utf8') as file:
+            file.write(rendered_page)
+        print('Site rebuilt')
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+if __name__ == '__main__':
+    main()

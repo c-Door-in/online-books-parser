5817d54b872160720eda94889b85f3b708b5531c
c_Door in <egor1@mail.ru>
2022-04-20 12:27:06 +0300

    Split books by two columns

--- render_website.py
+++ render_website.py
@@ -1,9 +1,11 @@
 import json
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from pprint import pprint
 from urllib.parse import urljoin
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+from more_itertools import chunked
 
 
 def get_index_template():
@@ -21,13 +23,16 @@ def main():
     with open('./parsing_result/books.json', 'r', encoding='utf8') as file:
         books = json.load(file)
     category_url = 'https://tululu.org/l55/'
+    
     for book in books:
         book['image_url'] = urljoin(category_url, book['image_url'])
-
+    
+    books_pairs = list(chunked(books, 2))
+    pprint(books_pairs)
     
     def on_reload():
         rendered_page = get_index_template().render(
-            books=books,
+            books_pairs = books_pairs
         )
         with open('index.html', 'w', encoding='utf8') as file:
             file.write(rendered_page)
